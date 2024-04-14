3db7cffe4a07d24128fb2b6e5caf0884e03326ea
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2024-04-15 00:40:55 +0530

    Update [searchQuery].js

--- pages/api/search/[searchQuery].js
+++ pages/api/search/[searchQuery].js
@@ -1,8 +1,8 @@
 const ytsr = require('ytsr');
 
-export default async(req, res) => {
+export default async (req, res) => {
     const { searchQuery } = req.query
-     const searchResults = await ytsr(searchQuery)
+    const searchResults = await ytsr(searchQuery, { limit: 10 })
 
     res.json(searchResults)
 }
