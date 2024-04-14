9cbf344ae14b00bff1c0c8636df346b4fad16aa8
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2024-04-15 00:13:30 +0530

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
 			<Html lang='en'>
 				<Head>
 					{/* PWA primary color */}
-					<meta name='theme-color' content={theme.palette.primary.main} />
+					<meta name='theme-color' />
 					<link
 						rel='stylesheet'
 						href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
@@ -65,7 +65,7 @@ MyDocument.getInitialProps = async ctx => {
 		// Styles fragment is rendered after the app and page rendering finish.
 		styles: [
 			...React.Children.toArray(initialProps.styles),
-			sheets.getStyleElement()
+			// sheets.getStyleElement()
 		]
 	}
 }
